dad5c97665b88cc33df46d3d802b229239e99420
Maxime Gammaitoni <maxime.gammaitoni@technologypartner.io>
2021-05-01 19:38:57 +0100

    feature/change_Andros_place

---
+++ Assets/Andros/Scripts/Managers/StatesManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using States;
+using Zenject;
+
+public class StatesManager
+{
+    BaseState currentState;
+
+    public BaseState CurrentState { 
+        get
+        {
+            return currentState;
+        }
+        set
+        {
+            currentState?.Out();
+            currentState = value;
+            currentState.In();
+        }
+    }
+
+    public void ChangeCurrentState(BaseState state)
+    {
+        CurrentState = state;
+    }
+}
